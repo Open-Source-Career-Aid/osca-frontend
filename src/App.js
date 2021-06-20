3b81c21c611b6974e66e273c51b5323aa4905704
Bucky25 <mayankkoshta2000@gmail.com>
2021-06-20 11:20:14 +0530

    created

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Header/>
+        <Route to="/" exact render={() => <Home/>} />
+        <Footer/>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MyHelperCompnents/MyMobile.js
@@ -0,0 +1,24 @@
+import React, { useState, useLayoutEffect } from 'react'
+
+const MyMobile = (props) => {
+    const [windowSize, setWindowSize] = useState(0);
+
+    useLayoutEffect(() => {
+        function UpdateSize() {
+            setWindowSize(window.innerWidth);
+        }
+        window.addEventListener("resize", UpdateSize);
+        UpdateSize();
+        return () => {
+            cleanup
+        };
+    }, [])
+
+    return (
+        <React.Fragment>
+            {windowSize <= 550 ? props.chaildren : null}
+        </React.Fragment>
+    )
+}
+
+export default MyMobile
