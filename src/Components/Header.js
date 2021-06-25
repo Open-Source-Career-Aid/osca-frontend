224df729df699130fe841861d261113f555dcfab
AkshitGuptaIITR <Gakshit92@gmail.com>
2021-06-25 22:24:37 +0530

    Mobile Header Added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ import "../Styles/Header.css";
 const Header = () => {
 
     const [scroll, setScroll] = useState(0);
-    const [drawerOpen, setDrawerOpen] = useState(0);
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
     useLayoutEffect(() => {
         function UpdateScroll() {
@@ -67,17 +67,66 @@ const Header = () => {
             </MyDesktop>
             <MyMobile>
                 <div
-                    className={scroll < 30 ? "header" : "header header-s"}
+                    className={scroll < 30 ? "header pt-2" : "header pt-2 header-s"}
                 >
                     <div className="row">
-                        <div className="col-1">
-                            
+                        <div className="col-3 text-center">
+                            <div className="menu-btn" onClick={() => { setDrawerOpen(!drawerOpen) }}>
+                                <div className={drawerOpen ? "change-1" : "bar-1"}></div>
+                                <div className={drawerOpen ? "change-2" : "bar-2"}></div>
+                                <div className={drawerOpen ? "change-3" : "bar-3"}></div>
+                            </div>
                         </div>
-                        <div className="col-4"></div>
+                        <div className="col-2"></div>
                         <div className="col-1 header-text">
                             OSCA
                         </div>
                     </div>
+                    {
+                        drawerOpen ? (
+                            <>
+                                <div className="row pt-5 header-ex">
+                                    <div className="justify-content-center text-center header-text">
+                                        <Link className="link">
+                                            Learn a Skill
+                                        </Link>
+                                    </div>
+                                </div>
+                                <hr className="header-bar" />
+                                <div className="row pt-2 header-ex">
+                                    <div className="justify-content-center text-center header-text">
+                                        <Link className="link">
+                                            Track Your Progress
+                                        </Link>
+                                    </div>
+                                </div>
+                                <hr className="header-bar" />
+                                <div className="row pt-2 header-ex">
+                                    <div className="justify-content-center text-center header-text">
+                                        <Link className="link">
+                                            About us
+                                        </Link>
+                                    </div>
+                                </div>
+                                <hr className="header-bar" />
+                                <div className="row pt-2 header-ex">
+                                    <div className="justify-content-center text-center header-text">
+                                        <Link className="link">
+                                            Login
+                                        </Link>
+                                    </div>
+                                </div>
+                                <hr className="header-bar" />
+                                <div className="row py-2 header-ex">
+                                    <div className="justify-content-center text-center header-text">
+                                        <Link className="link">
+                                            Sign Up
+                                        </Link>
+                                    </div>
+                                </div>
+                            </>
+                        ) : null
+                    }
                 </div>
             </MyMobile>
         </>
