bd7a7f39ac92a12d22687fc0b25238cb005b8998
Abhinav Agarwal <agarwalabhinav309@gmail.com>
2021-06-29 18:21:23 +0530

    error page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './Components/Header';
 import { LowerBody } from './Components/Homepage/LowerBody';
 import { Footer } from './Components/Footer';
 import Home from './Components/Homepage/Home';
+import { ErrorPage } from './Components/ErrorPage';
 
 function App() {
   return (
     <>
       <Router>
         <Header />
-        <Route to="/" exact render={() => <Home />} />
-        <LowerBody />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+            <LowerBody />
+          </Route>
+          <Route exact path="/error" render={() => {
+            return (
+              <ErrorPage />
+            );
+          }}>
+          </Route>
+        </Switch>
         <Footer />
       </Router>
     </>
