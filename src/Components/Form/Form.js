a2d165225cc782a45fa77886bbe995e4657217d3
Bucky25 <mayankkoshta2000@gmail.com>
2021-07-06 22:25:52 +0530

    changes

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -116,14 +116,14 @@ const RoadmapForm = () => {
             <Form onSubmit={e => handelSubmit(e)}>
                
                 <Row className='spacebetween'>
-                    <Col xs={12} sm={12} md={4} lg={4} xl={5} >
+                    <Col xs={12} sm={12} md={5} lg={5} xl={5} >
                         <Form.Group controlId='formName'>
                             <Form.Label>Name</Form.Label>
                             <Form.Control required type='text' value={state.name} placeholder='Enter your name' onChange={handleChangeName} />
                         </Form.Group>
                     </Col>
                    
-                    <Col className='formgroup' xs={12} sm={12} md={4} lg={4} xl={5} >
+                    <Col className='formgroup' xs={12} sm={12} md={5} lg={5} xl={5} >
                         <Form.Group controlId='intiuteName'>
                             <Form.Label>Organization Name</Form.Label>
                             <Form.Control required type='text' readOnly value={state.organization_name} placeholder='Enter organization' onChange={handleChangeOrganizationName} />
@@ -132,13 +132,13 @@ const RoadmapForm = () => {
                 </Row>
                 <br/>
                 <Row className='spacebetween'>
-                    <Col xs={12} sm={12} md={4} lg={4} xl={5}  >
+                    <Col xs={12} sm={12} md={5} lg={5} xl={5}  >
                         <Form.Group controlId='branchName'>
                             <Form.Label>Branch Name</Form.Label>
                             <Form.Control required type='text' value={state.branch_name} placeholder='Enter Your Branch ( Mechanical, CSE,.. )' onChange={handleChangeBranchName} />
                         </Form.Group>
                     </Col>
-                    <Col className='formgroup' xs={12} sm={12} md={4} lg={4} xl={5} >
+                    <Col className='formgroup' xs={12} sm={12} md={5} lg={5} xl={5} >
                         <Form.Group controlId='Year'>
                             <Form.Label>Year</Form.Label>
                             <Form.Control required type='text' value={state.program_duration} placeholder='2018 - 22' onChange={handleChangeyear} />
