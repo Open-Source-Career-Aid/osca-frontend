5cf674e574913f544c9f2c70baa344c0edc8317a
Akshit <Gakshit92@gmail.com>
2021-08-23 11:18:12 +0530

    Fetch request Addition in search data

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col, Form, InputGroup } from 'react-bootstrap';
 import SearchIcon from '@material-ui/icons/Search';
 import { IconButton } from '@material-ui/core';
@@ -6,6 +6,7 @@ import MyDesktop from "../../MyHelperCompnents/MyDesktop";
 import { makeStyles } from '@material-ui/styles';
 import "../../Styles/Search.css"
 const SearchBar = () => {
+    const [typed, setTyped] = useState('');
 
     const useStyle = makeStyles({
         black: {
@@ -14,6 +15,17 @@ const SearchBar = () => {
     });
 
     const classes = useStyle();
+
+    const url = `http://osca-api.herokuapp.com/form/learn-skill/?searchData=${typed}`;
+
+    console.log(url);
+
+    const fetchData = async () => {
+        const requestData = await fetch(url);
+        const data = requestData.json();
+        console.log(data)
+        return data;
+    };
 
     return (
         <Row className="top-pad">
@@ -28,9 +40,10 @@ const SearchBar = () => {
                         type="text"
                         className="Search"
                         placeholder="Search a Skill"
+                        onChange={(e) => setTyped(e.target.value)}
                     />
                     <InputGroup.Append className="search-btn">
-                        <IconButton aria-label="Search" size="small">
+                        <IconButton aria-label="Search" size="small" onClick={() => fetchData()}>
                             <SearchIcon className={classes.black} />
                         </IconButton>
                     </InputGroup.Append>
