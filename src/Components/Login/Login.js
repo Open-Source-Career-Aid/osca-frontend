a62c38b465d09ffa9b39f6b95ce6e242997be3b1
Akshit <Gakshit92@gmail.com>
2021-12-03 11:58:41 +0530

    Google auth complete

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,29 @@
 import React from "react";
 import style from "./Login.module.css";
 import google from "../../Images/google.png";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const Login = () => {
+  const signInWithGoogle = () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className={style.body}>
       <div className={style.card}>
         <h1>Login/Signup</h1>
-        <button>
+        <button onClick={signInWithGoogle}>
           <img src={google} className={style.googleImg} alt="" />
           <p>Continue with Google</p>
         </button>
